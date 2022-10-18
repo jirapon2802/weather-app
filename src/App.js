28f7e4ba3562208c5c408caaec70a9ef1b01dbb3
Jirapon <jirapon2802@gmail.com>
2022-10-19 00:24:59 +0700

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./page/weather";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
 }
 
 export default App;
